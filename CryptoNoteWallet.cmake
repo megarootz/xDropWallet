c3f0a327d9c11dfc3ce8322cd9112f82154232e8
megarootz <39173042+megarootz@users.noreply.github.com>
2018-08-26 05:58:15 +0800

    Update CryptoNoteWallet.cmake

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,4 +1,4 @@
-set(CN_PROJECT_NAME "xDrop")
-set(CN_CURRENCY_DISPLAY_NAME "xDrop")
-set(CN_CURRENCY_TICKER "DROP")
+set(CN_PROJECT_NAME "Xmynero")
+set(CN_CURRENCY_DISPLAY_NAME "Xmynero")
+set(CN_CURRENCY_TICKER "Xmyro")
 set(CN_VERSION 0.0.6-beta)
